3225f4d453eb89df48ab8ed6bff460c9218f6e74
aql123 <aiqiulin6699@163.com>
2022-11-11 10:42:33 +0800

    添加导航栏

--- zhiyu-demo1/Form1.Designer.cs
+++ zhiyu-demo1/Form1.Designer.cs
@@ -28,7 +28,116 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxTask = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBoxBrand = new System.Windows.Forms.ComboBox();
+            this.comboBoxStatus = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonFind = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.buttonOpen.Location = new System.Drawing.Point(947, 30);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(86, 33);
+            this.buttonOpen.TabIndex = 0;
+            this.buttonOpen.Text = "添加";
+            this.buttonOpen.UseVisualStyleBackColor = false;
+            this.buttonOpen.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(24, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "任务名称:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBoxTask
+            // 
+            this.textBoxTask.Location = new System.Drawing.Point(109, 37);
+            this.textBoxTask.Name = "textBoxTask";
+            this.textBoxTask.Size = new System.Drawing.Size(225, 21);
+            this.textBoxTask.TabIndex = 2;
+            this.textBoxTask.Text = "请输入任务名称";
+            this.textBoxTask.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBoxTask.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(377, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "手机品牌:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // comboBoxBrand
+            // 
+            this.comboBoxBrand.FormattingEnabled = true;
+            this.comboBoxBrand.ItemHeight = 12;
+            this.comboBoxBrand.Items.AddRange(new object[] {
+            "小米",
+            "华为",
+            "苹果",
+            "安卓"});
+            this.comboBoxBrand.Location = new System.Drawing.Point(462, 37);
+            this.comboBoxBrand.Name = "comboBoxBrand";
+            this.comboBoxBrand.Size = new System.Drawing.Size(137, 20);
+            this.comboBoxBrand.TabIndex = 4;
+            this.comboBoxBrand.Text = "请选择手机品牌";
+            this.comboBoxBrand.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBoxStatus
+            // 
+            this.comboBoxStatus.FormattingEnabled = true;
+            this.comboBoxStatus.ItemHeight = 12;
+            this.comboBoxStatus.Items.AddRange(new object[] {
+            "正常",
+            "异常",
+            "停止"});
+            this.comboBoxStatus.Location = new System.Drawing.Point(677, 37);
+            this.comboBoxStatus.Name = "comboBoxStatus";
+            this.comboBoxStatus.Size = new System.Drawing.Size(119, 20);
+            this.comboBoxStatus.TabIndex = 6;
+            this.comboBoxStatus.Text = "请选择状态";
+            this.comboBoxStatus.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(624, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "状态:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // buttonFind
+            // 
+            this.buttonFind.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.buttonFind.Location = new System.Drawing.Point(855, 30);
+            this.buttonFind.Name = "buttonFind";
+            this.buttonFind.Size = new System.Drawing.Size(86, 33);
+            this.buttonFind.TabIndex = 7;
+            this.buttonFind.Text = "查找";
+            this.buttonFind.UseVisualStyleBackColor = false;
+            this.buttonFind.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
@@ -36,13 +145,33 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoScroll = true;
             this.ClientSize = new System.Drawing.Size(1161, 524);
+            this.Controls.Add(this.buttonFind);
+            this.Controls.Add(this.comboBoxStatus);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBoxBrand);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.textBoxTask);
             this.Name = "Form1";
             this.Text = "梦网任务数据处理";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxTask;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBoxBrand;
+        private System.Windows.Forms.ComboBox comboBoxStatus;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonFind;
     }
 }
 
--- zhiyu-demo1/Form1.cs
+++ zhiyu-demo1/Form1.cs
@@ -16,5 +16,67 @@ namespace zhiyu_demo1
         {
             InitializeComponent();
         }
+
+
+        //添加文件的按钮
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //任务名称标签
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //请输入任务名称
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        //手机品牌标签
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //手机品牌选择
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //梦网数据处理
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //状态标签
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //查找按钮
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //状态选择
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
